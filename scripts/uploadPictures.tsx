1da074ab26dacbb55d4e4e9c742280437627cdfc
jonathan.rubstein <jrubstein@jonathans-mbp.speedport.ip>
2018-04-02 14:36:29 +0200

    Add more pictures

--- scripts/uploadPictures.tsx
+++ scripts/uploadPictures.tsx
@@ -12,7 +12,7 @@ cloudinary.config({
     const picturesFolder = path.join(__dirname, '..', '..', 'pictures')
     const files = await fs.readdirSync(picturesFolder)
     files.forEach(async (name) => {
-        if (name.indexOf('profile_cropped') !== 0) {
+        if (name.indexOf('.') === 0 || name === 'old') {
             return
         }
         console.log(await cloudinary.uploader.upload(path.join(picturesFolder, name)))
